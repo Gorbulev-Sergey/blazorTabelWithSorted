077bcdf111d1f7fe628c521bfcbbbcfba044b4fc
Серёжа <@GORBULEVSV-PC>
2021-12-13 21:15:48 +0300

    Добавьте файлы проекта.

---
+++ Models/book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blazorTabelWithSorted.Models
+{
+    public class book
+    {
+        public int id { get; set; }
+        public string author { get; set; }
+        public string title { get; set; }
+    }
+}
---
+++ Models/person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blazorTabelWithSorted.Models
+{
+    public class person
+    {
+        public int id { get; set; }
+        [Display(Name = "имя")]
+        public string name { get; set; }
+        [Display(Name = "фамилия")]
+        public string lastname { get; set; }
+        [Display(Name = "возраст")]
+        public int age { get; set; }
+    }
+}
